93cc125015f3d35533a5c0e2463ccb6d086b9a07
chentian <chentian@kayak.com>
2023-03-31 18:16:18 +0800

    修改

--- src/api/v1/user.js
+++ src/api/v1/user.js
@@ -5,7 +5,7 @@ const router = new Router()
 
 router.get('/test', (ctx, next) => {
   // ctx.router available
-  new ParameterException()
+  throw new ParameterException()
   ctx.body = { name: 'chentian', age: 20 }
 })
 
--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const exception = require('./middleware/exception')
 const app = new Koa()
 app.use(serve(resolve(__dirname, './static')))
 app.use(user.routes())
-app.use(exception())
+app.use(exception)
 app.listen(3009, () => {
   console.log('server listener at http://localhost:3009')
 })
