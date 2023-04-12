3cb90efa218289d0a2395e9e3e14c4e42087a43e
chentian <chentian@kayak.com>
2023-04-12 15:48:16 +0800

    feat: 自动挂载路由/配置/错误生成器

---
+++ src/core/bootstrap.js
@@ -0,0 +1,29 @@
+const requireDirectory = require('require-directory')
+const config = require('@/config')
+const exception = require('@/core/httpException')
+
+// 挂载系统配置
+function loadConfig() {
+  global.config = config
+}
+
+// 自动挂载路由
+function loadRouter(app) {
+  const visitor = (obj) => {
+    app.use(obj.routes())
+  }
+  requireDirectory(module, `${process.cwd()}/src/api/v1`, { visit: visitor })
+}
+
+// 挂载异常生成器
+function loadExecption() {
+  global.err = exception
+}
+
+function bootstrap(app) {
+  loadConfig()
+  loadRouter(app)
+  loadExecption()
+}
+
+module.exports = bootstrap
