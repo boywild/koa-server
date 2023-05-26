1a76bcad18e16f6472c2d8ff724033b9b66e8706
chentian <chentian@kayak.com>
2023-05-26 15:27:16 +0800

    feat: 增加接口鉴权

--- src/api/v1/book.js
+++ src/api/v1/book.js
@@ -1,8 +1,9 @@
 const Router = require('@koa/router')
+const Auth = require('@/middleware/auth')
 
 const router = new Router({ prefix: '/v1/book' })
 
-router.get('/hot_list', async (ctx) => {
+router.get('/hot_list', new Auth(8).m(), async (ctx) => {
   ctx.body = {
     name: '测试书',
     price: 12
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,53 @@
-async function auth(ctx, next) {
-  try {
-    await next()
-  } catch (e) {
-    console.log(e)
+const auth = require('basic-auth')
+const jwt = require('jsonwebtoken')
+const config = require('@/config')
+
+class Auth {
+  constructor(level) {
+    this.level = level
+  }
+
+  m() {
+    return async (ctx, next) => {
+      const token = auth(ctx.req)
+      let errMsg = 'token不合法'
+      let decode = {}
+      if (!token || !token.name) {
+        throw new global.err.Forbidden(errMsg)
+      }
+      try {
+        decode = jwt.verify(token.name, config.security.key)
+      } catch (e) {
+        if (e.name === 'TokenExpiredError') {
+          errMsg = 'token已过期'
+        }
+        if (e.name === 'JsonWebTokenError') {
+          errMsg = 'token无效'
+        }
+        throw new global.err.Forbidden(errMsg)
+      }
+
+      if (decode.scope < this.level) {
+        errMsg = '权限不足'
+        throw new global.err.Forbidden(errMsg)
+      }
+
+      ctx.auth = {
+        scope: decode.scope,
+        token: decode.token
+      }
+
+      next()
+    }
+  }
+
+  static verifyToken(token) {
+    try {
+      jwt.verify(token, config.security.key)
+      return true
+    } catch (e) {
+      return false
+    }
   }
 }
-
-module.exports = auth
+module.exports = Auth
